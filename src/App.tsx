3791ead26a8b024ef33e9342c4825aa31dbc09f9
Andrea Sansone <andreasansone35@gmail.com>
2023-07-20 16:47:49 +0200

    added vercel web analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { inject } from "@vercel/analytics";
 
+inject();
 export default function App() {
   return (
     <div className="max-w-7xl mx-auto">
